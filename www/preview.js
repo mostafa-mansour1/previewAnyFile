ac71e74133dde64c6f361bef5034063b55255621
ealejandromt <53374715+ealejandromt@users.noreply.github.com>
2021-04-15 10:42:13 -0500

    Correct cordova exec call
    And plugin function params order

--- www/preview.js
+++ www/preview.js
@@ -1,28 +1,28 @@
 
-var exec = require( "cordova/exec" );
+var cordova = require( "cordova" );
 
 var PreviewAnyFile = function () {
 
 };
 PreviewAnyFile.prototype.preview = function ( path, successCallback, errorCallback ) {
     console.warn( "preview method has been deprecated, kindly use previewPath, previewBase64 or previewAsset" )
-    exec( successCallback, errorCallback, "PreviewAnyFile", "preview", [ path ] );
+    cordova.exec( successCallback, errorCallback, "PreviewAnyFile", "preview", [ path ] );
 };
 
-PreviewAnyFile.prototype.previewBase64 = function ( successCallback, errorCallback, base64, opt = {} ) {
+PreviewAnyFile.prototype.previewBase64 = function ( base64, successCallback, errorCallback, opt = {} ) {
     let name = !!opt && opt.name ? opt.name : '';
     let mimeType = !!opt && opt.mimeType ? opt.mimeType : '';
-    exec( successCallback, errorCallback, "PreviewAnyFile", "previewBase64", [ base64, name, mimeType ] );
+    cordova.exec( successCallback, errorCallback, "PreviewAnyFile", "previewBase64", [ base64, name, mimeType ] );
 };
 
-PreviewAnyFile.prototype.previewPath = function ( successCallback, errorCallback, path, opt = {} ) {
+PreviewAnyFile.prototype.previewPath = function ( path, successCallback, errorCallback, opt = {} ) {
     let name = !!opt && opt.name ? opt.name : '';
     let mimeType = !!opt && opt.mimeType ? opt.mimeType : '';
 
-    exec( successCallback, errorCallback, "PreviewAnyFile", "previewPath", [ path, name, mimeType ] );
+    cordova.exec( successCallback, errorCallback, "PreviewAnyFile", "previewPath", [ path, name, mimeType ] );
 };
 
-PreviewAnyFile.prototype.previewAsset = function ( successCallback, errorCallback, path, opt = {} ) {
+PreviewAnyFile.prototype.previewAsset = function ( path, successCallback, errorCallback, opt = {} ) {
     let name = !!opt && opt.name ? opt.name : '';
     let mimeType = !!opt && opt.mimeType ? opt.mimeType : '';
     if ( !!path ) path = window.location.origin + path;
@@ -35,7 +35,7 @@ PreviewAnyFile.prototype.previewAsset = function ( successCallback, errorCallbac
             reader.onloadend = function () {
                 let base64 = reader.result;
                 if ( !name ) name = path.split( '/' ).pop();
-                exec( successCallback, errorCallback, "PreviewAnyFile", "previewBase64", [ base64, name, mimeType ] );
+                cordova.exec( successCallback, errorCallback, "PreviewAnyFile", "previewBase64", [ base64, name, mimeType ] );
             }
         } )
         .catch( () => console.log( 'assets error' ) );
